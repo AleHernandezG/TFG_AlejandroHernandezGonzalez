9b87cb5de7b564842b7e2950c9200ecf834f7b5e
Alejandro Hernandez <alejes@usal.es>
2026-05-18 17:23:38 +0200

    Problemas en un PR.3

--- frontend/src/services/ingredientesService.ts
+++ frontend/src/services/ingredientesService.ts
@@ -1,7 +1,7 @@
 import type { DatoIngrediente } from '@/config/ingredientes'
 
 export const ingredientesService = {
-  buscarEdamam: async (): Promise<DatoIngrediente[]> => {
+  buscarEdamam: async (_query: string): Promise<DatoIngrediente[]> => {
     // TODO Fase 6: llamada real a Edamam Food Database API
     return []
   },
